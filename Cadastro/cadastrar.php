92b42de21b8e483c7fafebef6cbe56c909fc4fd9
ISMAEL <ismaelsantanateixeira@gmail.com>
2025-05-27 23:33:32 -0300

    Conexão com banco de dados

---
+++ Cadastro/cadastrar.php
@@ -0,0 +1,33 @@
+<?php
+$host = "localhost"; // ou 127.0.0.1
+$user = "root"; // ou o nome do usuário do seu MySQL
+$pass = "4840Senai**"; // sua senha do MySQL
+$dbname = "wtask";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Erro: " . $conn->connect_error);
+}
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+$telefone = $_POST['telefone'];
+$nivel = "clie";
+
+
+$sql = "INSERT INTO cadastro (nome, email, senha, telefone, nivel) VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssss", $nome, $email, $senha, $telefone, $nivel);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo "Cadastro realizado!";
+} else {
+    echo "Erro ao cadastrar.";
+}
+
+$stmt->close();
+$conn->close();
+?>
