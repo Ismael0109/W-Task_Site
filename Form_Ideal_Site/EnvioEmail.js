634af38a5a0de8cdcae9adb5d461657565d5426b
ISMAEL <ismaelsantanateixeira@gmail.com>
2025-06-17 10:30:23 -0300

    bug email resolvido

--- Form_Ideal_Site/EnvioEmail.js
+++ Form_Ideal_Site/EnvioEmail.js
@@ -1,21 +1,28 @@
 function enviarEmail(){
-    
-    const nome = document.getElementById("nomeEmpresa").value;
-    const cnpj = document.getElementById("cnpjEmpresa").value;
-    const tipoEmpresa = document.getElementById("Opcoes").value;
-    const outroTipo = document.getElementById("OtrTipo").value;
-    const desc = document.getElementById("txtDesc").value;
+    if ((document.getElementById("nomeEmpresa").value == "")
+    || document.getElementById("cnpjEmpresa").value == "" || document.getElementById("Opcoes").value == "Selecione" || document.getElementById("txtDesc").value == "") {
 
-    const destinatario = "contatowtesk@gmail.com";
-    const assunto = encodeURIComponent("Novo formulário de contato e Idealização de site de " + nome);
-    let corpo;
-    if (tipoEmpresa == "Outro Tipo") {
-        corpo = encodeURIComponent("Nome: " + nome + "\nCNPJ: " + cnpj + "\nTipo de Empresa: " + outroTipo +"\n\nDescrição:\n" + desc);
+        alert('Preencha Todos os Campos');
+
     } else {
-        corpo = encodeURIComponent("Nome: " + nome + "\nCNPJ: " + cnpj + "\nTipo de Empresa: " + tipoEmpresa +"\n\nDescrição:\n" + desc);
-    }
+        const nome = document.getElementById("nomeEmpresa").value;
+        const cnpj = document.getElementById("cnpjEmpresa").value;
+        const tipoEmpresa = document.getElementById("Opcoes").value;
+        const outroTipo = document.getElementById("OtrTipo").value;
+        const desc = document.getElementById("txtDesc").value;
+
+        const destinatario = "contatowtesk@gmail.com";
+        const assunto = encodeURIComponent("Novo formulário de contato e Idealização de site de " + nome);
+        let corpo;
+        if (tipoEmpresa == "Outro Tipo") {
+            corpo = encodeURIComponent("Nome: " + nome + "\nCNPJ: " + cnpj + "\nTipo de Empresa: " + outroTipo +"\n\nDescrição:\n" + desc);
+        } else {
+            corpo = encodeURIComponent("Nome: " + nome + "\nCNPJ: " + cnpj + "\nTipo de Empresa: " + tipoEmpresa +"\n\nDescrição:\n" + desc);
+        }
     
 
     window.location.href = `mailto:${destinatario}?subject=${assunto}&body=${corpo}`;
     
 }
+    }
+    
